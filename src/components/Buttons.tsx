57fc451f27408d731ec8783e67434fa30d8b97d8
Samaar Ahmed <diamantbilvard@gmail.com>
2023-02-06 17:29:36 +0100

    Initial commit

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button'
+import React from 'react'
+
+const Buttons = ()=>{
+    return(
+        <div>
+        <h2>Hello from buttons</h2>
+        <Button variant="contained">Add new to do</Button>
+
+        </div>
+    )
+}
+export default Buttons
