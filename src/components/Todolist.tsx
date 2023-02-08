64bfd196866d96d63fc03ba0bd557b533cbbdc5f
Samaar Ahmed <diamantbilvard@gmail.com>
2023-02-08 16:29:54 +0100

    input and button working

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,15 +1,30 @@
 import { Button, Card, Input } from "@mui/material"
+import { useState } from "react"
+
 
 function Todolist(){
+    const todoarr = [""]
+    const [todos,setTodo] = useState(todoarr)
+    const [text,setText] = useState('')
+
+    const  AddtoDo = ()=>{
+
+
+
+        setTodo([...todos,text])
+
+
+    }
+    
     return(
-        <div>
-        <Card>
+        <div className="container">
+        <Card style={{backgroundColor: "lightgray"}}>
         <h2>Lägg till saker i listan!</h2>
         <div>
-        <input type="text"/>
+        <input  value={text} onChange={event=>(setText(event.target.value))}/> 
         </div>
-        <Button variant="contained">Add new to do</Button>
-
+        <Button variant="contained" onClick={AddtoDo}>Add new to do</Button>
+        <Display todos={todos}></Display>
         </Card>
 
         </div>
@@ -17,3 +32,15 @@ function Todolist(){
     )
 }
 export default Todolist
+
+interface Itodo {
+    todos : string[]
+}
+
+function Display ({todos}:Itodo){
+    return(<>
+        {todos.map(todo=><li>{todo}</li>)}
+ </>
+    )
+
+}
