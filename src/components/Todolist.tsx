ec736c944ed55979809796a82e547e4cc0cae5f0
Samaar Ahmed <diamantbilvard@gmail.com>
2023-02-15 16:55:47 +0100

    onlick true false and linethrough working

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,26 +1,40 @@
-import { Button, Card, Input } from "@mui/material"
+import { Button, Card, IconButton, Input, ListItem, ListItemText } from "@mui/material"
 import { useState } from "react"
+import DeleteIcon from '@mui/icons-material/Delete';
+import { List } from "@mui/icons-material";
+import { style } from "@mui/system";
 
-const todoarr = [{id:1, title:"Plugga"},{id:2, title:"spela"}];
+const todoarr = [{id:1, title:"Plugga",status:false},{id:2, title:"spela",status:false}];
 
 function Todolist(){
     const [todos,setTodo] = useState(todoarr)
-    const [text,setText] = useState({id:3,title:""})
+    const [text,setText] = useState({id:3,title:"",status:false})
 
     const  AddtoDo = ()=>{
 
-
         setTodo([...todos,text])
-        console.log(todoarr)
+        console.log(todos)
 
 
     }
     
-    const todolist = todos.map((todo)=>(
+    function overline (id:number){
+
+        const updatedlinethrough = todos.map((mytodo)=>{
+            if(id==mytodo.id){
+                return{...mytodo,status:!mytodo.status}
+            } return mytodo
+        });
+
+        setTodo(updatedlinethrough)
+        console.log(updatedlinethrough)
+
+    }
+   /* const todolist = todos.map((todo)=>(
 
         <Display key={todo.id} id = {todo.id} title={todo.title}></Display>
 
-        ));
+        ));*/
 
     return(
         <div className="container">
@@ -28,26 +42,83 @@ function Todolist(){
         <div>
         <input  value={text.title} onChange={event=>setText({...text,title: event.target.value})} placeholder="Att göra:"/> 
         </div>
-        <Button variant="contained" onClick={AddtoDo}>Add new to do</Button>
-        {todolist}
+        <Button variant="contained" fullWidth onClick={AddtoDo}>Add new to do</Button>
+        <div className="container">
+
+      <table className="table table-striped">
+      <tbody>
+      {todos.map((todo)=>(
+          <tr key={todo.id}>
+            <td className={todo.status == true? "addline" : "withoutline"} key={todo.id} onClick = {()=>overline(todo.id)}>{todo.title} <button aria-label="delete" onClick={()=>deleteTodo(todo.id)}>
+  <DeleteIcon />
+</button>
+</td>
+          </tr>
+                  ))}
+
+      </tbody>
+
+      </table>
+    </div>
+
         
         </div>
 
     )
+
+    function deleteTodo(id: number){
+
+        const updatedtods = todos.filter(item => item.id != id)
+        setTodo(updatedtods)
+        console.log(updatedtods)
+
+    }
+    /*
+    <List dense={dense}>
+    {generate(
+      <ListItem
+        secondaryAction={
+          <IconButton edge="end" aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        }
+      >
+        <ListItemText
+          primary="Single-line item"
+          secondary={secondary ? 'Secondary text' : null}
+        />
+      </ListItem>,
+    )}
+  </List>
+*/
+
+
 }
 export default Todolist
 
-interface Itodo {
+/*interface Itodo {
     id : number;
     title: string;
 }
 
+
 function Display ({id,title}:Itodo){
     return(<>
-        <li>
-            id:{id}  title : {title}
-        </li>
+        <div className="container">
+      <table className="table table-striped">
+      <tbody>
+          <tr>
+            <td>{title} <IconButton aria-label="delete" onClick={deleteTodo(todo.id)}>
+  <DeleteIcon />
+</IconButton>
+</td>
+          </tr>
+      </tbody>
+
+      </table>
+    </div>
+
 </>
     )
 
-}
+}*/
